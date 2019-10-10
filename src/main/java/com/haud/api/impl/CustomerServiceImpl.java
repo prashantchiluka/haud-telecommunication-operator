58ae4bfdb42a1febb78e536d527210b67384c25d
Amir Ansari <amir.ansari@neosofttech.com>
2019-10-10 14:03:21 +0530

    added logs in service

--- src/main/java/com/haud/api/impl/CustomerServiceImpl.java
+++ src/main/java/com/haud/api/impl/CustomerServiceImpl.java
@@ -11,8 +11,11 @@ import com.haud.entity.SimCard;
 import com.haud.repository.CustomerRepository;
 import com.haud.repository.SimCardRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Transactional
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
 	
 	@Autowired
@@ -24,10 +27,12 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public long addCustomer(Customer customer,String userName) {
 
+		log.info("in service impl, adding customer " + customer);
+		
 		customer.setCreatedBy(userName);
 		customer.setUpdatedBy(userName);
 
-		
+		log.info("in service impl, adding customer, repository call ");
 		return customerRepository.save(customer).getId();
 
 	}
@@ -35,6 +40,8 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public void updateCustomer(long customerId,List<Long> simId,String userName) {
 		 
+		log.info("in service impl, linking simcard to customer, sim card id: "+ simId + "customer id: "+ customerId);
+		
 		Customer customer = customerRepository.findById(customerId).get();
 		List<SimCard> simCards = simCardRepository.findAllById(simId);
 		
@@ -43,17 +50,24 @@ public class CustomerServiceImpl implements CustomerService {
 			simCard.setUpdatedBy(userName);
 		});
 		customer.setUpdatedBy(userName);
+		
+		log.info("in service impl, linked simcard to customer");
 	}
 
 	@Override
 	public Customer getCustomer(long customerId){
 		
+		log.info("in service impl, getting specific customer with id: "+customerId+ "repository call");
+		
 		Customer customer = customerRepository.findById(customerId).get();
+		log.info("customer fetched from database, with customerid: " + customerId);
 		return customer;
 	}
 
 	@Override
 	public boolean isCustomerExist(long customerId) {
+		
+		log.info("in service impl isCustomerExist customerId: "+ customerId);
 		return customerRepository.existsById(customerId);
 		}
 	
--- src/main/java/com/haud/api/impl/SimCardServiceImpl.java
+++ src/main/java/com/haud/api/impl/SimCardServiceImpl.java
@@ -6,8 +6,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.haud.entity.SimCard;
 import com.haud.repository.SimCardRepository;
+
+import lombok.extern.slf4j.Slf4j;
 @Service
 @Transactional
+@Slf4j
 public class SimCardServiceImpl implements SimCardService{
 
 	@Autowired
@@ -16,9 +19,12 @@ public class SimCardServiceImpl implements SimCardService{
 	@Override
 	public long createSimCard(SimCard simCard,String userName) {
 		
+		log.info("in service impl, adding simcard " + simCard);
+		
 		simCard.setCreatedBy(userName);
 		simCard.setUpdatedBy(userName);
 		
+		log.info("in service impl, adding simcard, repository call ");
 		return simCardRepository.save(simCard).getId();
 	}
 }
